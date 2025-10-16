34570211f9c92b8da0acdcedd64b219fbae03cb4
Dmitry Kovyazin <it@brandup.online>
2025-10-16 12:16:29 +0700

    fix NativeSetup

--- InteropAssemblies/interoplib/interoplib_interoplib_WiFi.cpp
+++ InteropAssemblies/interoplib/interoplib_interoplib_WiFi.cpp
@@ -31,8 +31,8 @@ static void wifi_event_handler(void *arg, esp_event_base_t event_base, int32_t e
 
 void WiFi::NativeSetup( const char* ssid, const char* password, HRESULT &hr )
 {
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    // ESP_ERROR_CHECK(esp_netif_init());
+    // ESP_ERROR_CHECK(esp_event_loop_create_default());
 
 	/* Initialize WiFi */
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
